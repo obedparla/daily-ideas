dc33ae4e80209ddc1fe0fad412d62da1a764ae7f
Obed Marquez Parlapiano <obed.marquezp@gmail.com>
2018-11-30 22:37:25 +0100

    Add more functions to the firebase class. Add the firebase provider.

--- src/Firebase/firebase.js
+++ src/Firebase/firebase.js
@@ -1,4 +1,5 @@
 import app from 'firebase/app';
+import 'firebase/auth';
 
 const config = {
   apiKey: process.env.REACT_APP_API_KEY,
@@ -12,7 +13,22 @@ const config = {
 class Firebase {
   constructor(){
     app.initializeApp(config);
+
+    this.auth = app.auth();
   }
+
+  createUserWithEmailAndPassword = (email, password) =>
+    this.auth.createUserWithEmailAndPassword(email, password);
+
+  signInWithEmailAndPassword = (email, password) =>
+    this.auth.signInWithEmailAndPassword(email, password);
+
+  signOut = () => this.auth.signOut();
+
+  resetPassword = email => this.auth.sendPasswordResetEmail(email);
+
+  updatePassword = password =>
+    this.auth.currentUser.updatePassword(password);
 }
 
 export default Firebase;
--- src/index.js
+++ src/index.js
@@ -6,9 +6,9 @@ import * as serviceWorker from './serviceWorker';
 import {Firebase, FirebaseContext} from './Firebase';
 
 ReactDOM.render(
-  <FirebaseContext value={new Firebase()}>
+  <FirebaseContext.Provider value={new Firebase()}>
     <App/>
-  </FirebaseContext>
+  </FirebaseContext.Provider>
   , document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
