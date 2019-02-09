aadc8c0e12f42309f1c7f2f55cef3e77ce8e8912
Obed Parlapiano <obed.marquezp@gmail.com>
2019-02-09 14:44:07 +0100

    Add Sigined in and out navigation

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -5,19 +5,36 @@ import Button from "@material-ui/core/Button";
 import * as ROUTES from "../../constants/routes";
 import SignOutButton from "../SignOut";
 
-const Navigation = () => (
-  <>
-    <Link to={ROUTES.LANDING}>
-      <Button color="secondary">Landing</Button>
-    </Link>
-    <Link to={ROUTES.SIGN_IN}>
-      <Button color="secondary">Login</Button>
-    </Link>
-    <Link to={ROUTES.SIGN_UP}>
-      <Button color="secondary">Sign up</Button>
-    </Link>
-    <SignOutButton />
-  </>
+const Navigation = ({ authUser }) => (
+  <div>{authUser ? <NavigationAuth /> : <NavigationNonAuth />}</div>
+);
+
+const NavigationAuth = () => (
+  <ul>
+    <li>
+      <Link to={ROUTES.LANDING}>Landing</Link>
+    </li>
+    <li>
+      <Link to={ROUTES.HOME}>Home</Link>
+    </li>
+    <li>
+      <Link to={ROUTES.ACCOUNT}>Account</Link>
+    </li>
+    <li>
+      <SignOutButton />
+    </li>
+  </ul>
+);
+
+const NavigationNonAuth = () => (
+  <ul>
+    <li>
+      <Link to={ROUTES.LANDING}>Landing</Link>
+    </li>
+    <li>
+      <Link to={ROUTES.SIGN_IN}>Sign In</Link>
+    </li>
+  </ul>
 );
 
 export default Navigation;
