b56e07e832057c319fa47f7eb216d8fbe214f25f
Obed <obed.imp@gmail.com>
2019-02-24 19:17:13 +0100

    Add ideas endpoints

--- src/Firebase/firebase.js
+++ src/Firebase/firebase.js
@@ -1,6 +1,6 @@
-import app from 'firebase/app';
-import 'firebase/auth';
-import 'firebase/database';
+import app from "firebase/app";
+import "firebase/auth";
+import "firebase/database";
 
 const config = {
   apiKey: process.env.REACT_APP_API_KEY,
@@ -8,7 +8,7 @@ const config = {
   databaseURL: process.env.REACT_APP_DATABASE_URL,
   projectId: process.env.REACT_APP_PROJECT_ID,
   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID
 };
 
 class Firebase {
@@ -17,6 +17,7 @@ class Firebase {
 
     this.auth = app.auth();
     this.db = app.database();
+    this.app = app;
 
     /* Social Sign In Method Provider */
     this.googleProvider = new app.auth.GoogleAuthProvider();
@@ -30,18 +31,15 @@ class Firebase {
   signInWithEmailAndPassword = (email, password) =>
     this.auth.signInWithEmailAndPassword(email, password);
 
-  signInWithGoogle = () =>
-    this.auth.signInWithPopup(this.googleProvider);
+  signInWithGoogle = () => this.auth.signInWithPopup(this.googleProvider);
 
-  signInWithFacebook = () =>
-    this.auth.signInWithPopup(this.facebookProvider);
+  signInWithFacebook = () => this.auth.signInWithPopup(this.facebookProvider);
 
   signOut = () => this.auth.signOut();
 
   resetPassword = email => this.auth.sendPasswordResetEmail(email);
 
-  updatePassword = password =>
-    this.auth.currentUser.updatePassword(password);
+  updatePassword = password => this.auth.currentUser.updatePassword(password);
 
   // *** Merge Auth and DB User API *** //
 
@@ -49,7 +47,7 @@ class Firebase {
     this.auth.onAuthStateChanged(authUser => {
       if (authUser) {
         this.user(authUser.uid)
-          .once('value')
+          .once("value")
           .then(snapshot => {
             const dbUser = snapshot.val();
 
@@ -64,7 +62,7 @@ class Firebase {
               email: authUser.email,
               emailVerified: authUser.emailVerified,
               providerData: authUser.providerData,
-              ...dbUser,
+              ...dbUser
             };
 
             next(authUser);
@@ -74,14 +72,19 @@ class Firebase {
       }
     });
 
+  // ** Utils **
+
   // *** User API ***
   user = uid => this.db.ref(`users/${uid}`);
-  users = () => this.db.ref('users');
+  users = () => this.db.ref("users");
 
   // *** Todos api *** //
 
-  ideas = (userId) => this.db.ref(`ideas/${userId}`);
-  idea = (userId, todoId) => this.db.ref(`ideas/${userId}/${todoId}`);
+  ideas = (userId, date) => this.db.ref(`ideas/${userId}/${date}`);
+  ideaTitle = (userId, date, title) =>
+    this.db.ref(`ideas/${userId}/${date}/title`);
+  idea = (userId, date, todoId) =>
+    this.db.ref(`ideas/${userId}/${date}/${todoId}`);
 }
 
 export default Firebase;
